19551b6a7476e94220b4ad1672305d31e2ec778a
npoxop <npoxop7@gmail.com>
2020-12-04 17:49:06 +0300

    Таблица умножения

---
+++ src/vtb/baranovdv/Main.java
@@ -0,0 +1,66 @@
+package vtb.baranovdv;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // таблица умножения
+        int one=1;
+        for (int oneplus = 1; oneplus <= 10; oneplus++)
+        System.out.println(one + " * " + oneplus + " = " + one*oneplus);
+
+        System.out.println(" ");
+
+        int two=2;
+        for (int twoplus = 1; twoplus <= 10; twoplus++)
+        System.out.println(two + " * " + twoplus + " = " + two*twoplus);
+
+        System.out.println(" ");
+
+        int three=3;
+        for (int threeplus = 1; threeplus <= 10; threeplus++)
+        System.out.println(three + " * " + threeplus + " = " + three*threeplus);
+
+        System.out.println(" ");
+
+        int four=4;
+        for (int fourplus = 1; fourplus <= 10; fourplus++)
+        System.out.println(four + " * " + fourplus + " = " + four*fourplus);
+
+        System.out.println(" ");
+
+        int five=5;
+        for (int fiveplus = 1; fiveplus <= 10; fiveplus++)
+        System.out.println(five + " * " + fiveplus + " = " + five*fiveplus);
+
+        System.out.println(" ");
+
+        int six=6;
+        for (int sixplus = 1; sixplus <= 10; sixplus++)
+        System.out.println(six + " * " + sixplus + " = " + six*sixplus);
+
+        System.out.println(" ");
+
+        int seven=7;
+        for (int sevenplus = 1; sevenplus <= 10; sevenplus++)
+        System.out.println(seven + " * " + sevenplus + " = " + seven*sevenplus);
+
+        System.out.println(" ");
+
+        int eigth=8;
+        for (int eigthplus = 1; eigthplus <= 10; eigthplus++)
+        System.out.println(eigth + " * " + eigthplus + " = " + eigth*eigthplus);
+
+        System.out.println(" ");
+
+        int nine=9;
+        for (int nineplus = 1; nineplus <= 10; nineplus++)
+        System.out.println(nine + " * " + nineplus + " = " + nine*nineplus);
+
+        System.out.println(" ");
+
+        int ten=10;
+        for (int tenplus = 1; tenplus <= 10; tenplus++)
+        System.out.println(ten + " * " + tenplus + " = " + ten*tenplus);
+    }
+
+}
